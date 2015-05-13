8e19450e8f2e92dc6c793215213f67a60e59c429
jim leach <yogajim@gmail.com>
2015-05-13 15:36:34 -0400

    Homework6 update

---
+++ functions.js
@@ -0,0 +1,70 @@
+// Q1
+// Write a function called `sum` that takes two
+// parameters and returns the sum of those 2 numbers.
+
+function sum(x, y) {
+  return = x + y;
+
+
+}
+
+
+// Q2
+// Write a function named `avg` that takes 3 parameters
+// and returns the average of those 3 numbers.
+
+function avg (x, y, z) {
+  return = (x+y+z) / 3
+}
+
+
+// Q3
+// Write a function called `getLength` that takes one
+// parameter (a string) and returns the length
+
+function getLength(elvis) {
+  return (elvis).length;
+
+}
+
+
+// Q4
+// Write a function called `greaterThan` that takes
+// two parameters and returns `true` if the second
+// parameter is greater than the first. Otherwise
+// the function should return `false`.
+
+function greaterThan(one, two) {
+ if (two > one){
+  return true;
+ }else;
+ {return false}
+}
+
+
+// Q5
+// Write a function called `greet` that takes a
+// single parameter and returns a string that
+// is formated like "Hello, Name!" where *Name*
+// is the parameter that was passed in.
+
+function greet (yourName) {
+  return ("Hello, " + yourName +"!");
+}
+
+
+// Q6
+// Write a function called `madlib` that takes
+// 4 or more parameters (words). The function
+// should insert the words into a pre-defined
+// sentence. Finally the function should return
+// that sentence.
+// Note: When I say words and sentence I mean
+// strings. For example:
+// words: "quick", "fox", "fence"
+// sentence: "quick brown fox jumps over the fence"
+
+function madlib(a, b, c, d) {
+  return (a + " brown " + b + " fox " + c + " jumps " + d  + " over the fence");
+}
+
---
+++ readme.md
@@ -0,0 +1,1 @@
+Functions practice Assignment 6
